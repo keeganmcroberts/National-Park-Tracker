b3a70eacb3bac837a20770ba7ef074120d324e95
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-10-27 15:50:01 -0500

    react router and useNav

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,6 +4,7 @@ function Header(){
             <h1>Pro Football Figures</h1>
             <div className="page-banner">
                 <ul className="banner-links">
+                    <ul className="links"><a  href="/">Home</a></ul>
                     <ul className="links"><a  href="/teams">Teams</a></ul>
                     <ul className="links"><a  href="/players">Players</a></ul>
                     <ul className="links"><a  href="/leagueLeaders">Leage Leaders</a></ul>        
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,4 +1,27 @@
 function Home(){
+
+    const scheduleKey = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'b4a431490bmsh7ccab2b032763d3p18e9acjsnbf25af2a7eeb',
+            'X-RapidAPI-Host': 'nfl-schedule.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://nfl-schedule.p.rapidapi.com/v1/schedules', scheduleKey)
+        .then(response => response.json())
+        .then(response => console.log("schedule:", response))
+        .catch(err => console.error(err));
+
+
+        
+        fetch('https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=32')
+            .then(response => response.json())
+            .then(response => console.log("athletes", response))
+            .catch(err => console.error(err));
+
+
+
     return(
         <div className="page">
             <h1>Home</h1>
