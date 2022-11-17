f6630d8eb0c6731ff88c5caa135fc6cb84027624
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-11-17 10:36:36 -0600

    editing home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,5 +1,6 @@
 import ParkDetailPage from './parkDetailPage';
 import React, { useRef, useEffect, useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2VlZ2FuLW1jcm9iZXJ0cyIsImEiOiJjbGExZmVwdnEwMnF3M3BranY2eG51bmdvIn0.ZZtanHWCPYfhDObnypq7VA';
 
@@ -9,9 +10,9 @@ function Home({parkData}){
 
 
     const [map, setMap] = useState(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const [lng, setLng] = useState(-94.578331);
+    const [lat, setLat] = useState(39.099724);
+    const [zoom, setZoom] = useState(2);
     
     const ref = useRef(null);
 
@@ -35,16 +36,27 @@ function Home({parkData}){
         setMap(map);
       }
     }, []);
+
+    let navigate = useNavigate();
+    
+    function viewPark(parkCode){
+        navigate(`/park/${parkCode}`)
+    }
     
     return(
-        <div className="parkPage">
-        {parkData.map(eachPark=>{
-            if(eachPark.designation === "National Park")
-            return (
-                // <ParkDetailPage key={eachPark.id} eachPark={eachPark}/>
-                <></>
-            )
-        })}
+        <div className='homepage'>
+            <div ref={ref} className="home-map-container"> </div>
+            <div className="parkPage">   
+            {parkData.map(eachPark=>{
+                if(eachPark.designation === "National Park")
+                return (
+                    <div>
+                        <h5>{eachPark.fullName}</h5>
+                        <img onClick={() => viewPark(eachPark.parkCode)}  className="homepage-images" src={eachPark.images[0].url}></img>
+                    </div>
+                )
+            })}
+            </div>
         </div>
     )
 }
