b25d475b9290de6925d68e8f6fccb9ff49ecf7c0
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-12-08 15:52:43 -0600

    added User controller for user authorizaiton and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :users
   get "sign_up", to: "registrations#new"
+  get "about", to: "about#index"
+  get "/", to: "main#index"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
