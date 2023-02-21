bc223d57b7624cdb629ec3b50e052882640b7c92
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-02-21 14:07:12 -0600

    added user submit for comment with prevent default, need comments to persist and add post request to userComment controller

--- client/src/components/review.js
+++ client/src/components/review.js
@@ -3,23 +3,38 @@ import React, { useRef, useEffect, useState } from 'react';
 
 function Review(){
     const [comment, setComment] = useState('comment...');
+    const [newComment, setNewComment] = useState([]);
 
-    const handleChange = event => {
+    function handleChange( event ) {
         setComment(event.target.value);
       };
 
       console.log(comment)
 
+      function handleSubmit(event){
+          event.preventDefault()
+
+          newComment.unshift(comment)
+      }
+
+      console.log("comment array", newComment)
 
     return(
         <div className='comment-form'>
             <br></br>
             <br></br>
             <br></br>
-        <form>
+        <form onSubmit={handleSubmit}>
         <input className="comment-input" name="comment" type="text" value={comment} onChange={handleChange}/>
         <input type="submit"/>
         </form>
+        <div>
+           {newComment.map(comments=>{
+               return(
+                   <h1>{comments}</h1>
+               )
+           })}
+        </div>
         </div>
     )
 }
